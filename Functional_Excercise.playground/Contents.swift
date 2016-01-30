30024d1bc04c740b9862097e2a341e70233f59bf
Kent <kent@kefbytes.com>
2016-01-30 18:51:12 -0500

    Added completed function exercises playground

--- Functional_Excercise.playground/Contents.swift
+++ Functional_Excercise.playground/Contents.swift
@@ -66,3 +66,32 @@ var domains = getEnabledCustomerDomains()
 Our task is to refactor the four methods above using a more functional style of programming. In the process we will conforming to the DRY principle.
 */
 
+// Start by creating a function that takes a single String parameter named fieldName and uses a long if else block to build the array.
+func getEnabledCustomerFields(fieldName: String) -> [String] {
+    var returnedArray = [String]()
+    for customer in customers {
+        if customer.enabled {
+            if fieldName == "name" {
+               returnedArray.append(customer.name)
+            } else if fieldName == "state" {
+                returnedArray.append(customer.state)
+            } else if fieldName == "contact" {
+                returnedArray.append(customer.primaryContact)
+            } else if fieldName == "domain" {
+                returnedArray.append(customer.domain)
+            }
+
+        }
+    }
+    return returnedArray
+}
+
+// try calling out new function and passing in the field names
+names = getEnabledCustomerFields("name")
+states = getEnabledCustomerFields("state")
+primaryContacts = getEnabledCustomerFields("contact")
+domains = getEnabledCustomerFields("domain")
+
+
+
+
