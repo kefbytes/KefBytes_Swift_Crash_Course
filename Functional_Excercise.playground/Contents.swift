d29951e037c6842324b153e8d1222d01a3fbb9a0
Franks, Kent <kfranks@cardinalsolutions.com>
2016-02-04 15:22:10 -0500

    Completing functional_exercise.playground

--- Functional_Excercise.playground/Contents.swift
+++ Functional_Excercise.playground/Contents.swift
@@ -128,4 +128,148 @@ getEnabledCustomerFields(customerName)
 /*:
 What's the problem with the way the code stands now? Well to start we would need to create objects for each of the field types we want to return (name, state, domain, contact). And pass it to our getEnabledCustomerFields function call.
 */
+// Let's change our getEnabledCustomerFields function, renaming it getEnabledCustomerFieldsFromFunction, the parameter will be a function of type (Customer) -> String. We'll call this function passing the customer from the current iteration of the loop
+func getEnabledCustomerFieldsFromFunction(fieldFunction: (Customer) -> String) -> [String] {
+    var returnedArray = [String]()
+    for customer in customers {
+        if customer.enabled {
+            returnedArray.append(fieldFunction(customer))
+        }
+    }
+    return returnedArray
+}
+
+// Now instead of creating a class we can create functions and pass them. See how we are moving away from object oriented and more towards functional?
+func getCustomerState(customer: Customer) -> String {
+    return customer.state
+}
+
+// Now just call our getEnabledCustomerFieldsFromFunction passing in the function that we want to use.
+getEnabledCustomerFieldsFromFunction(getCustomerState)
+
+/*:
+But hold on there, we're still creating a buynch of functions. Why do we need to do that? Well we don't, why not create the function on the fly when we are calling getEnabledCustomerFieldsFromFunction() and pass it in as a closure?
+
+In a closure you omit the curly braces and insert the 'in' keyword before the body of the function. The closure itself is not named and only includes the function type and body. So the syntax is something like this 
+
+functionType in body
+
+Where functionType is any type you would like that may or may not include parameters and a return type, and body is what ever actions/logic that function is supposed to perform.
+*/
+// Call the getEnabledCustomerFieldsFromFunction function passing in a closure that is the function type and body of the getCustomerState function.
+getEnabledCustomerFieldsFromFunction({
+    (customer: Customer) -> String in return customer.state
+})
+
+// And of course we can make this a trailing closure for better readability. Move the closure outside of and after the ()
+getEnabledCustomerFieldsFromFunction(){
+    (customer: Customer) -> String in return customer.state
+}
+
+/*:
+Now anytime you want to call the getEnabledCustomerFieldsFromFunction function you just pass in the closure you want to use.
+*/
+// We've gotten the customer states array not get the array for customer name, primary contact and domain.
+getEnabledCustomerFieldsFromFunction(){
+    (customer: Customer) -> String in return customer.name
+}
+
+getEnabledCustomerFieldsFromFunction(){
+    (customer: Customer) -> String in return customer.primaryContact
+}
+
+getEnabledCustomerFieldsFromFunction(){
+    (customer: Customer) -> String in return customer.domain
+}
+
+/*: 
+So far so good, we are getting functional. Now let's shift gears slightly. What if we wanted to get a list/array of all customers that are enabled? The actual customer objects not just a field from the object?
+Well we'd have to write a second function similar to our  getEnabledCustomerFieldsFromFunction function. But instead of filling our array with field values we'd be filling them with customer objects.
+*/
+// Our function would look something like this
+func getEnabledCustomerInfo(fieldFunction: (Customer) -> Customer) -> [Customer] {
+    var returnedArray = [Customer]()
+    for customer in customers {
+        if customer.enabled {
+            returnedArray.append(fieldFunction(customer))
+        }
+    }
+    return returnedArray
+}
+
+/*:
+If we look closely we can see that this function is very similar to our other function. Wouldn't it be great if we could use the same function for both cases?
+
+This can be accomplished easily using generics. We just define a generic parameter type and use that as a placeholder to replace the return type of the parameter function, the return type of our getEnabledCustomers function and the type of array we want to return.
+*/
+// Redo our getEnabledCustomers function using generics. Set a generic parameter type  <someParameterType> before the parameter list. Then use it as a placeholder for the rest of the function.
+func getEnabledCustomerInfo<T>(fieldFunction: (Customer) -> T) -> [T] {
+    var returnedArray = [T]()
+    for customer in customers {
+        if customer.enabled {
+            returnedArray.append(fieldFunction(customer))
+        }
+    }
+    return returnedArray
+}
+
+// Call the getEnabledCustomers function passing in a closure that just returns the customer object
+getEnabledCustomerInfo(){
+    (customer: Customer) -> Customer in return customer
+}
+
+// Call the getEnabledCustomers function to get the arrays filled with the various field values that we've been previously using (name, state, primaryContact and domain)
+getEnabledCustomerInfo(){
+    (customer: Customer) -> String in return customer.name
+}
+
+getEnabledCustomerInfo(){
+    (customer: Customer) -> String in return customer.state
+}
+
+getEnabledCustomerInfo(){
+    (customer: Customer) -> String in return customer.primaryContact
+}
+
+getEnabledCustomerInfo(){
+    (customer: Customer) -> String in return customer.domain
+}
+
+/*:
+Pretty exciting! Now we have one function that returns either a value or object and we just call that function passing in a closure.
+
+What if we wanted to prepend or postfix something on one of the field values we are returning? Simple just do it in the closure you are passing.
+*/
+// Call or getEnabledCustomers function and return an array of states, but postfix the state with a colon and then USA. Example: NC:USA
+getEnabledCustomerInfo(){
+    (customer: Customer) -> String in return "\(customer.state):USA"
+}
+
+/*:
+Let's make one last adjustment. What if we wanted to get all the same information we already are getting but for customers who are disabled? Would we right a second function? Of course not. We'd pass in a function that tested whether or not the customer was enabled or disabled and return the appropriate list.
+*/
+// Let's change our getEnabledCustomerInfo again and rename it getCustomerInfo. We want to add a second parameter named isEnabled that is a function type of (Customer) -> Bool. Then we replace our if customer.enabled check with a call to the isEnabled function.
+func getCustomerInfo<T>(fieldFunction: (Customer) -> T, isEnabled: (Customer) -> Bool) -> [T] {
+    var returnedArray = [T]()
+    for customer in customers {
+        if isEnabled(customer) {
+            returnedArray.append(fieldFunction(customer))
+        }
+    }
+    return returnedArray
+}
+
+// Now when we call the getCustomerInfo function we need to pass a closure specifying if we want enabled or disabled customers. keep in mind that only the last closure can be a trailing closure.
+getCustomerInfo({
+    (customer: Customer) -> String in return customer.name})
+    { (customer: Customer) -> Bool in return customer.enabled // returns enalbled customers
+}
+
+getCustomerInfo({
+    (customer: Customer) -> String in return customer.name})
+    { (customer: Customer) -> Bool in return !customer.enabled // returns disabled customers
+}
+
+
+
 
